9c5e2da9d126ec0bbaf0db3f20a5da8e2031da11
Mateus Pinto da Silva <mateusps10@outlook.com>
2021-10-28 11:33:15 -0300

    format

--- pages/create_user/create_user.js
+++ pages/create_user/create_user.js
@@ -1,29 +1,28 @@
-$(document).ready(function(){
-    $("#register_form").submit(function(event){
+$(document).ready(function () {
+  $("#register_form").submit(function (event) {
+    event.preventDefault();
 
-        event.preventDefault();
+    var form_data = {
+      user_name: $("#user_name").val(),
+      user_email: $("#user_email").val(),
+      user_password: $("#user_password").val(),
+      security_question: $("#security_question").val(),
+      answer_question: $("#answer_question").val(),
+    };
 
-        var form_data={
-            user_name:$('#user_name').val(),
-            user_email:$('#user_email').val(),
-            user_password:$('#user_password').val(),
-            security_question:$('#security_question').val(),
-            answer_question:$('#answer_question').val()
-        }
-    
-        $.ajax({
-            url: "http://localhost/tp_web/crud/create/create_user.php",
-            type: "POST",
-            data: {data_request:form_data}
-        }).done(function(resposta) {
-            console.log(resposta);
-    
-        }).fail(function(jqXHR, textStatus ) {
-            console.log("Request failed: " + textStatus);
-    
-        }).always(function() {
-            console.log("completou");
-        });
-    });
+    $.ajax({
+      url: "http://localhost/tp_web/crud/create/create_user.php",
+      type: "POST",
+      data: { data_request: form_data },
+    })
+      .done(function (resposta) {
+        console.log(resposta);
+      })
+      .fail(function (jqXHR, textStatus) {
+        console.log("Request failed: " + textStatus);
+      })
+      .always(function () {
+        console.log("completou");
+      });
+  });
 });
-
